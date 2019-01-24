395fb7d9a8e050cbb9094b4af541b4b0cbb5c3a6
Adrian <apgomez85@gmail.com>
2019-01-24 12:33:39 -0800

    final

---
+++ src/components/AddExpensePage.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const AddExpensePage = () => <div>Create expense component</div>;
+
+export default AddExpensePage;
---
+++ src/components/EditExpensePage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const EditExpensePage = props => (
+  <div>Edit page component of id {props.match.params.id}</div>
+);
+
+export default EditExpensePage;
