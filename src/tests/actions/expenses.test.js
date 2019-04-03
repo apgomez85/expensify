ad362ce8e8489e040fa1d2f9900dfd23363ce0bb
Adrian <apgomez85@gmail.com>
2019-04-03 07:13:29 -0700

    jest testing

---
+++ src/tests/actions/expenses.test.js
@@ -0,0 +1,9 @@
+import { removeExpense, editExpense, addExpense } from '../../actions/expenses';
+
+test('should setup remove expense action object', () => {
+  const action = removeExpense({ id: '123abc' });
+  expect(action).toEqual({
+    type: 'REMOVE_EXPENSE',
+    id: '123abc'
+  });
+});
