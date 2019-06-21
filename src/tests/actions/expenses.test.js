04ad5a82d276a40d15838500ebc08c3811d246e7
Adrian <apgomez85@gmail.com>
2019-06-21 08:33:06 -0700

    startEditExpense

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -7,7 +7,8 @@ import {
   addExpense,
   setExpenses,
   startSetExpenses,
-  startRemoveExpense
+  startRemoveExpense,
+  startEditExpense
 } from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
 import database from '../../firebase/firebase';
@@ -145,6 +146,29 @@ test('should remove expense from firebase', done => {
     })
     .then(snapshot => {
       expect(snapshot.val()).toBeFalsy();
+      done();
+    });
+});
+
+test('should edit expense from firebase', done => {
+  const store = createMockStore({});
+  const id = expenses[0].id;
+  const updates = { note: 'edited note' };
+
+  store
+    .dispatch(startEditExpense(id, updates))
+    .then(() => {
+      const actions = store.getActions();
+      expect(actions[0]).toEqual({
+        type: 'EDIT_EXPENSE',
+        id,
+        updates
+      });
+
+      return database.ref(`expenses/${id}`).once('value');
+    })
+    .then(snapshot => {
+      expect(snapshot.val().note).toBe(updates.note);
       done();
     });
 });
